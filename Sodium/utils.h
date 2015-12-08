9c769329708266f876af1ff80704b00a33c885c9
Frank Denis <github@pureftpd.org>
2015-12-08 18:11:23 +0100

    libsodium update to 1.0.7

--- Sodium/utils.h
+++ Sodium/utils.h
@@ -41,7 +41,13 @@ int sodium_compare(const unsigned char *b1_, const unsigned char *b2_,
             __attribute__ ((warn_unused_result));
 
 SODIUM_EXPORT
+int sodium_is_zero(const unsigned char *n, const size_t nlen);
+
+SODIUM_EXPORT
 void sodium_increment(unsigned char *n, const size_t nlen);
+
+SODIUM_EXPORT
+void sodium_add(unsigned char *a, const unsigned char *b, const size_t len);
 
 SODIUM_EXPORT
 char *sodium_bin2hex(char * const hex, const size_t hex_maxlen,
