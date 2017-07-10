5aa5eae4e4c7077fb6f111f789a02a21205f19a1
Frank Denis <github@pureftpd.org>
2017-07-10 16:09:18 +0200

    Be consistent with the existing code
    Use correct key lengths.
    
    Padding with zeros is less surprising than spaces.
    
    Don't invent a new way to return errors.

--- Sodium/KeyDerivation.swift
+++ Sodium/KeyDerivation.swift
@@ -11,6 +11,10 @@ import libsodium
 
 public class KeyDerivation {
     public typealias Key = Data
+    public let BytesMin = Int(crypto_kdf_bytes_min())
+    public let BytesMax = Int(crypto_kdf_bytes_max())
+    public let KeyBytes = Int(crypto_kdf_keybytes())
+    public let ContextBytes = Int(crypto_kdf_contextbytes())
 
     /**
      Derives a subkey from the specified input key. Each index (from 0 to (2^64) - 1) yields a unique deterministic subkey.
@@ -27,50 +31,39 @@ public class KeyDerivation {
      - Note: Input and output keys must have a length between 16 and 64 bytes (inclusive), otherwise an error is thrown. Context must be at most 8 characters long. If the specified context is shorter than 8 characters, it will be padded to 8 characters.
 
      */
-    public func derive(secretKey: Key, index: UInt64, length: Int = crypto_kdf_keybytes(), context: String) throws -> Key {
-        if length < crypto_kdf_bytes_min() {
-            throw NSError(domain: "libsodium", code: -10,
-                          userInfo: [NSLocalizedDescriptionKey: String(format:"the length of the derived key must be at least %d bytes", crypto_kdf_bytes_min())])
+    public func derive(secretKey: Data, index: UInt64, length: Int, context: String) throws -> Key? {
+        if length < BytesMin {
+            return nil
         }
 
-        if length > crypto_kdf_bytes_max() {
-            throw NSError(domain: "libsodium", code: -11,
-                          userInfo: [NSLocalizedDescriptionKey: String(format:"the length of the derived key must be less than or equal to %d bytes", crypto_kdf_bytes_max())])
+        if length > BytesMax {
+            return nil
         }
 
-        if secretKey.count < crypto_kdf_bytes_min() {
-            throw NSError(domain: "libsodium", code: -12,
-                          userInfo: [NSLocalizedDescriptionKey: String(format:"the length of the input key must be at least %d bytes", crypto_kdf_bytes_min())])
+        if secretKey.count != KeyBytes {
+            return nil
         }
 
-        if secretKey.count > crypto_kdf_bytes_max() {
-            throw NSError(domain: "libsodium", code: -13,
-                          userInfo: [NSLocalizedDescriptionKey: String(format:"the length of the input key must be less than or equal to %d bytes", crypto_kdf_bytes_max())])
+        var contextBin = [UInt8](context.utf8)
+        if contextBin.count > ContextBytes {
+            return nil
+        }
+        while contextBin.count < ContextBytes {
+            contextBin += [0]
         }
 
-        if context.lengthOfBytes(using: .utf8) > crypto_kdf_contextbytes() {
-            throw NSError(domain: "libsodium", code: -14,
-                          userInfo: [NSLocalizedDescriptionKey: String(format:"the length of the context String must be less than or equal to %d bytes", crypto_kdf_contextbytes())])
-        }
+        var subKey = Data(count: length)
 
-        let contextPadded = context.padding(toLength: crypto_kdf_contextbytes(), withPad: " ", startingAt: 0)
-
-        var subKey = Key(count: length)
-
-        let result = subKey.withUnsafeMutableBytes { (subKeyPtr) -> Int32 in
-            return secretKey.withUnsafeBytes { (secretKeyPtr) -> Int32 in
-                return crypto_kdf_derive_from_key(subKeyPtr, length, index, contextPadded, secretKeyPtr)
+        let result = subKey.withUnsafeMutableBytes { subKeyPtr in
+            return secretKey.withUnsafeBytes { secretKeyPtr in
+                return contextBin.withUnsafeBytes { contextBinPtr in
+                    return crypto_kdf_derive_from_key(subKeyPtr, length, index, contextBinPtr, secretKeyPtr)
+                }
             }
         }
-
         if result != 0 {
-            print("Error during key derivation: ", result)
-                throw NSError(domain: "libsodium", code: Int(result),
-                              userInfo: [NSLocalizedDescriptionKey: "error in underlying libsodium function while trying to derive subkey from master key",
-                                         "context": context,
-                                         "index": index])
+            return nil
         }
-
         return subKey
     }
 }
