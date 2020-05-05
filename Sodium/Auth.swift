75273577b20ceb8e1d128c9bf4d920ef7d214f3d
Justin Singer <justinsinger1@gmail.com>
2020-05-05 11:05:03 -0700

    Add hmacsha256 auth functions

--- Sodium/Auth.swift
+++ Sodium/Auth.swift
@@ -47,6 +47,47 @@ extension Auth {
             secretKey
         ).exitCode
     }
+
+    /**
+     Computes an authentication tag for a message using a key
+
+     - Parameter message: The message to authenticate.
+     - Parameter secretKey: The key required to create and verify messages.
+
+     - Returns: The computed authentication tag.
+     */
+    public func hmacsha256(message: Bytes, secretKey: SecretKey) -> Bytes? {
+        guard secretKey.count == KeyBytes else { return nil }
+
+        var tag = Array<UInt8>(count: Bytes)
+        guard .SUCCESS == crypto_auth_hmacsha256 (
+            &tag,
+            message, UInt64(message.count),
+            secretKey
+        ).exitCode else { return nil }
+
+        return tag
+    }
+
+    /**
+     Verifies that an authentication tag is valid for a message and a key
+
+     - Parameter message: The message to verify.
+     - Parameter secretKey: The key required to create and verify messages.
+     - Parameter tag: The authentication tag.
+
+     - Returns: `true` if the verification is successful.
+     */
+    public func hmacsha256_verify(message: Bytes, secretKey: SecretKey, tag: Bytes) -> Bool {
+        guard secretKey.count == KeyBytes else {
+            return false
+        }
+        return .SUCCESS == crypto_auth_hmacsha256_verify (
+            tag,
+            message, UInt64(message.count),
+            secretKey
+        ).exitCode
+    }
 }
 
 extension Auth: SecretKeyGenerator {
