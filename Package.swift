330cff0292618ac717df361d9341d83d98172ec6
Simon Kempendorf <simon@code28.de>
2020-11-19 18:52:50 +0100

    Use xcframework only for supported OS

--- Package.swift
+++ Package.swift
@@ -1,28 +1,31 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+var products: [Product] = [.library(name: "Sodium", targets: ["Sodium"])]
+var dependencies: [Package.Dependency] = []
+var targets: [Target] = [
+    .target(
+        name: "Sodium",
+        dependencies: ["Clibsodium"],
+        path: "Sodium",
+        exclude: ["libsodium", "Info.plist"]),
+    .testTarget(
+        name: "SodiumTests",
+        dependencies: ["Sodium"],
+        exclude: ["Info.plist"]),
+]
+
+
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+products.append(.library(name: "Clibsodium", targets: ["Clibsodium"]))
+targets.append(.binaryTarget(name: "Clibsodium", path: "Clibsodium.xcframework"))
+#else
+dependencies.append(.package(name: "Clibsodium", url: "https://github.com/TICESoftware/Clibsodium.git", from: "1.0.0"))
+#endif
+
 let package = Package(
     name: "Sodium",
-    products: [
-        .library(
-            name: "Clibsodium",
-            targets: ["Clibsodium"]),
-        .library(
-            name: "Sodium",
-            targets: ["Sodium"]),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "Clibsodium",
-            path: "Clibsodium.xcframework"),
-        .target(
-            name: "Sodium",
-            dependencies: ["Clibsodium"],
-            path: "Sodium",
-            exclude: ["libsodium", "Info.plist"]),
-        .testTarget(
-            name: "SodiumTests",
-            dependencies: ["Sodium"],
-            exclude: ["Info.plist"]),
-    ]
+    products: products,
+    dependencies: dependencies,
+    targets: targets
 )
