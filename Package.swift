d9ce2f0a2746f978ecd7d2c49f4fae2486f34567
Simon Kempendorf <simon@code28.de>
2020-11-24 13:15:18 +0100

    Include Clibsodium for linux into this repo

--- Package.swift
+++ Package.swift
@@ -1,31 +1,42 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-var products: [Product] = [.library(name: "Sodium", targets: ["Sodium"])]
-var dependencies: [Package.Dependency] = []
-var targets: [Target] = [
-    .target(
-        name: "Sodium",
-        dependencies: ["Clibsodium"],
-        path: "Sodium",
-        exclude: ["libsodium", "Info.plist"]),
-    .testTarget(
-        name: "SodiumTests",
-        dependencies: ["Sodium"],
-        exclude: ["Info.plist"]),
-]
-
-
+let clibsodiumTarget: Target
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-products.append(.library(name: "Clibsodium", targets: ["Clibsodium"]))
-targets.append(.binaryTarget(name: "Clibsodium", path: "Clibsodium.xcframework"))
+    clibsodiumTarget = .binaryTarget(
+        name: "Clibsodium",
+        path: "Clibsodium.xcframework")
 #else
-dependencies.append(.package(name: "Clibsodium", url: "https://github.com/TICESoftware/Clibsodium.git", from: "1.0.0"))
+    clibsodiumTarget = .systemLibrary(
+        name: "Clibsodium",
+        path: "Clibsodium",
+        pkgConfig: "libsodium",
+        providers: [
+            .apt(["libsodium-dev"]),
+            .brew(["libsodium"])
+        ])
 #endif
 
 let package = Package(
     name: "Sodium",
-    products: products,
-    dependencies: dependencies,
-    targets: targets
+    products: [
+        .library(
+            name: "Clibsodium",
+            targets: ["Clibsodium"]),
+        .library(
+            name: "Sodium",
+            targets: ["Sodium"]),
+    ],
+    targets: [
+        clibsodiumTarget,
+        .target(
+            name: "Sodium",
+            dependencies: ["Clibsodium"],
+            path: "Sodium",
+            exclude: ["libsodium", "Info.plist"]),
+        .testTarget(
+            name: "SodiumTests",
+            dependencies: ["Sodium"],
+            exclude: ["Info.plist"]),
+    ]
 )
