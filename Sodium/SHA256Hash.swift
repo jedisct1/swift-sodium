677f3f77f2f1549bbaba35a5982c867f975418ac
Andrzej Fiedukowicz <fiedukow@gmail.com>
2024-01-18 15:55:50 +0100

    Add support for SHA256

---
+++ Sodium/SHA256Hash.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Clibsodium
+
+public struct SHA256Hash {
+    public let Bytes = Int(crypto_hash_sha256_bytes())
+}
+
+extension SHA256Hash {
+    /**
+     Computes short but unpredictable (without knowing the secret key) values suitable for picking a list in a hash table for a given key.
+
+     - Parameter message: The data to be hashed.
+
+     - Returns: The computed SHA256 hash.
+     */
+    public func hash(message: Bytes) -> Bytes? {
+        var output = Array<UInt8>(count: Bytes)
+
+        guard .SUCCESS == crypto_hash_sha256 (
+            &output,
+            message, UInt64(message.count)
+        ).exitCode else { return nil }
+        return output
+    }
+}
