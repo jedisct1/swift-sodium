8eb04d22a24c185817fca2d0714c2e4ad7c0684d
Frank Denis <github@pureftpd.org>
2026-01-28 13:21:22 +0100

    Move the headers to Headers/Clibsodium

---
+++ Sources/Sodium/Sodium.h
@@ -0,0 +1,14 @@
+//
+//  Sodium.h
+//  Sodium
+//
+//  Umbrella header for the Sodium framework.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sodium.
+FOUNDATION_EXPORT double SodiumVersionNumber;
+
+//! Project version string for Sodium.
+FOUNDATION_EXPORT const unsigned char SodiumVersionString[];
